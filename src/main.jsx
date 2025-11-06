837994f4ed74407df78fff1dec481f2083f0ad7a
Andreas Bachmeier <bantho.official@gmail.com>
2025-11-06 22:15:25 +0100

    Revert "Inline PlainHive media assets as base64 data URIs"

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import('./data/plainhiveWordmarkBase64.js')
-  .then(({ default: wordmarkBase64 }) => {
-    const favicon = document.querySelector("link[rel='icon']#app-favicon");
-    if (favicon) {
-      favicon.href = `data:image/png;base64,${wordmarkBase64}`;
-    }
-  })
-  .catch((error) => {
-    console.error('Failed to apply PlainHive favicon', error);
-  });
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
